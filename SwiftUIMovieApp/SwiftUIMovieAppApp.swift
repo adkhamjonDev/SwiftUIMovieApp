645fa4aa0874279dd21e18293caf6fa774b2dea6
adkhamjon <adkhamjon.rakhimov.dev@gmail.com>
2024-11-21 17:21:14 +0500

    Initial Commit

---
+++ SwiftUIMovieApp/SwiftUIMovieAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIMovieAppApp.swift
+//  SwiftUIMovieApp
+//
+//  Created by Adkhamjon Rakhimov on 21/11/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIMovieAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
